6a0b7b9d0d117aa5d850c3acaab48cdddd0d18b5
Santino Mayan <127140452+Sanpa33@users.noreply.github.com>
2025-12-26 16:10:48 -0300

    agrega interfaz y funciones de Dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
-import './App.css'
+    /**
+     * Challenge: Update the array of numbers in state to be
+     * an array of objects instead. Each object should look like:
+     * { value: <random number>, isHeld: false }
+     * 
+     * Making this change will break parts of our code, so make
+     * sure to update things so we're back to a working state
+     */
 
-function App() {
-  
+import Die from './Die.jsx'
+import {useState} from 'react'
 
-  return (
-    <>
-      
-    </>
-  )
+
+export default function App() {
+    
+    function generateAllNewDice() {
+
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.floor(Math.random() * 6))
+        
+    }
+
+    const [dice, setDice] = useState(generateAllNewDice())
+
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+
+
+    const diceElements = dice.map((value, index) => (
+        <Die key={index} value={value} />
+    ))
+
+    
+
+
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+
+        </main>
+    )
 }
-
-export default App
